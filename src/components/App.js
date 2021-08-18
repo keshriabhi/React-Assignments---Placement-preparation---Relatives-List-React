443a2f0f5967a3508b90fcf6f3d476239c8bab08
Abhishek keshri <abhishekkeshri@live.com>
2021-08-18 20:39:30 +0530

    added keys in the generate list

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,17 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
+    state = ['Akshay', 'Satyendra', 'Sahil', 'Bhargavi', 'Mithun', 'Rohit'];
     render() {
-
+        var list = this.state.map((item,index) => {
+            return <li key={'relativeListItem'+(index+1)}>{item}</li>
+    })
         return(
             <div id="main">
                {/* Do not remove the main div */}
+               <ol>
+                   {list}
+               </ol>
             </div>
         )
     }
@@ -14,3 +20,15 @@ class App extends Component {
 
 
 export default App;
+
+// import React from 'react';
+// const List =(props)=>{
+//   const  myList=props.myList;
+//     const listItem=myList.map((myList,index)=><li key={'relativeListItem'+(index+1)}>{myList}</li>);
+//     return (
+//         <div>
+//            <ol>{listItem}</ol>  
+//         </div>
+//     )
+// };
+// export  default List;
